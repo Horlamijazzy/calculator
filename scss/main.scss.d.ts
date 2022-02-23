e2e99533f6bbdf48e56f29eb388ef764f51944b7
Horlamijazzy <roderrick007@gmail.com>
2022-02-23 16:56:36 +0100

    first

---
+++ scss/main.scss.d.ts
@@ -0,0 +1,10 @@
+import { StyleProp, TextStyle } from 'react-native'
+
+export const container: StyleProp<TextStyle>
+export const display: StyleProp<TextStyle>
+export const display-1: StyleProp<TextStyle>
+export const temp-result: StyleProp<TextStyle>
+export const all-button: StyleProp<TextStyle>
+export const button: StyleProp<TextStyle>
+export const button:hover: StyleProp<TextStyle>
+export const btn-0: StyleProp<TextStyle>
